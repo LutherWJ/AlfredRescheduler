d12a4e21dadea724475273cf921fd0aa6d44b160
Logan McGregor <youremail@example.com>
2026-01-29 17:32:54 -0500

    Building

--- src/domain/Rescheduler.java
+++ src/domain/Rescheduler.java
@@ -6,6 +6,9 @@ public class Rescheduler {
     
     public void reschedule(RegistrationSystem system, String courseID) {
         // Logic will go here:
+        //get the course number
+        //get all students in course
+        //get all courses students are taking without duplicates
 
     }
 }
