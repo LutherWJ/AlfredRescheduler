670c4664b681b5c0f84702e173b5eb5c50d9e7c9
lutherwj <williamluther62@gmail.com>
2026-01-29 16:12:33 -0500

    Fixed circular dependencies Created entities for storing data in memory as a hashmap Made MeetingTime into its own entity.

--- src/domain/Rescheduler.java
+++ src/domain/Rescheduler.java
@@ -4,14 +4,8 @@ import java.util.List;
 
 public class Rescheduler {
     
-    /**
-     * This method will contain the logic to reschedule courses.
-     * It uses the RegistrationSystem to access data.
-     */
-    public void reschedule(RegistrationSystem system) {
+    public void reschedule(RegistrationSystem system, String courseID) {
         // Logic will go here:
-        // 1. Iterate through courses or students
-        // 2. Check conflicts
-        // 3. Find available rooms
+
     }
 }
